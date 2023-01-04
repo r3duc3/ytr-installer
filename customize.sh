75ec2da07884ed0ac09923ee752b19d9040e334a
r3duc3 <itsreduce@gmail.com>
2023-01-04 19:25:44 +0700

    isntaller

--- customize.sh
+++ customize.sh
@@ -5,21 +5,35 @@ fi
 #####################
 # here's the config #
 REAL='/sdcard/YouTube.apk' # real youtube file
+MODIFIED='/data/adb/revanced.apk' # modified youtube
 #####################
-NAME='YouTube'
-IS_INSTALLED=$(pm list packages | grep 'com.google.android.youtube')
-IS_EXIST=$(ls -1 $REAL)
 
+if ! [ -f $MODIFIED ]; then
+	abort "[!] modified youtube not found"
+fi
+
+NAME='YouTube'
+INSTALLED=$(pm list packages | grep 'com.google.android.youtube')
+EXIST=$(ls -1 $REAL)
+
+# check real Youtube first
 ui_print "[+] Check real $NAME"
-if ! $IS_INSTALLED; then
+if ! $INSTALLED; then
 	ui_print "[-] Real $NAME not installed"
-	if ! $IS_EXIST; then
+	if ! $EXIST; then
 		abort "[!] file $REAL not exist"
 	fi
 	ui_print "[-] Installing real $NAME"
 	mmm_exec showLoading
-	#pm install --dont-kill -g $REAL
-	#mmm_exec hideLoading
+	pm install --dont-kill -g $REAL
+	mmm_exec hideLoading
 fi
+ui_print "[-] real $NAME installed"
 
-ui_print "[+] real Youtube was successful installed"
+# then patch
+REAL_BASE=$(pm path com.google.android.youtube | sed 's/package://g')
+REAL_PATH=$(dirname $REAL_BASE)
+MODIFIED_PATH="$MODPATH/$REAL_PATH"
+mkdir $MODIFIED_PATH
+cp $MODIFIED $MODIFIED_PATH
+ui_print "[+] revanced installed"
