8212fb8395ef6f9ed729d67c3f9587030731ab17
r3duc3 <itsreduce@gmail.com>
2023-01-05 21:51:51 +0700

    update create.sh

--- create.sh
+++ create.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-zip -r9v ytr-installer.zip customize.sh META-INF/ module.prop README.md service.sh uninstall.sh
+zip -r9v ytr-installer.zip META-INF/ CHANGELOG.md customize.sh module.prop README.md service.sh uninstall.sh update.json
