45fded8b7d618b45eda413ee0625fd7a9101be73
r3duc3 <itsreduce@gmail.com>
2023-01-06 20:19:36 +0700

    add ytr-config.prop

--- create.sh
+++ create.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-zip -r9v ytr-installer.zip META-INF/ CHANGELOG.md customize.sh module.prop README.md service.sh uninstall.sh
+zip -r9v ytr-installer.zip META-INF/ CHANGELOG.md customize.sh module.prop README.md service.sh uninstall.sh ytr-config.prop
