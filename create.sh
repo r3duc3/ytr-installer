841c1926debaa7347e6f8eefbc864b66d984a83c
r3duc3 <itsreduce@gmail.com>
2023-01-06 19:11:06 +0700

    remove update.json from zip

--- create.sh
+++ create.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-zip -r9v ytr-installer.zip META-INF/ CHANGELOG.md customize.sh module.prop README.md service.sh uninstall.sh update.json
+zip -r9v ytr-installer.zip META-INF/ CHANGELOG.md customize.sh module.prop README.md service.sh uninstall.sh
